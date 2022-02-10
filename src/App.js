5f1f6d8a239811b722ad6316c8bafa03b8915c33
allan <allan.queiroz.dev@gmail.com>
2022-02-10 20:41:56 -0300

    removed some unnecessary things; added the MenuBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import MenuBar from './components/MenuBar';
+
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <React.Fragment>
+      <MenuBar/>
+      <Container maxWidth="sm">
+        <h1>Hello worldd</h1>
+      </Container>
+    </React.Fragment>
   )
 }
 
