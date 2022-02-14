6e497eb9dd5ad654250439db8675853b83f4a3c1
allan <allan.queiroz.dev@gmail.com>
2022-02-14 12:24:26 -0300

    clicking in a pokemon card show his information

--- src/pages/detailed.js
+++ src/pages/detailed.js
@@ -1,20 +1,31 @@
 import React from "react";
 import pokeApi from "../services/pokeAPI";
 import { useParams } from "react-router-dom";
+import {Typography } from "@mui/material";
 
 const Detailed = () => {
-    const [getPokemonByID, setGetPokemonByID] = React.useState(null);
-    let {id} = useParams();
+    const [getPokemonByID, setGetPokemonByID] = React.useState([]);
+    const {id} = useParams();
 
     React.useEffect(() => {
-        setGetPokemonByID(id)
         pokeApi.get(`/pokemon/${id}`)
-            .then(pokemon => console.log(pokemon.data))
+            .then(pokemon => setGetPokemonByID([pokemon.data]))
             .catch(err => console.log(err))
     },[])
 
     return (
-        <h1> O cara clicou no pokemon ID: {getPokemonByID}</h1>
+        <React.Fragment>
+            {
+                getPokemonByID ? getPokemonByID.map(pokemon=>(<React.Fragment key={id}>
+                    <Typography variant="h4" component="div">Nome: {pokemon.name}</Typography>
+                    <Typography variant="h4" component="div">Nº Pokedex: {pokemon.id}</Typography>
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} />
+                    <Typography>Altura: {(pokemon.height)/10} m</Typography>
+                    <Typography>Peso: {(pokemon.weight)/10} Kg</Typography>
+                    <Typography>Exp. base: {pokemon.base_experience}</Typography>
+                </React.Fragment>)) : <h1>Loading</h1>
+            }
+        </React.Fragment>
     )
 }
 export default Detailed;
